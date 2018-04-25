1887e0c568045a27bb5b1a6b44d920724d61de48
Losthb <398516771@qq.com>
2018-04-25 16:46:50 +0800

    4.25.test2

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
+const path = require('path');
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
+const Handlebars = require('handlebars');
+const config = require('../config/defaultConfig');
+
+const tplPath = path.join(__dirname,'../template/dir.tpl');
+const source = fs.readFileSync(tplPath);
+const template = Handlebars.compile(source.toString());
 
 module.exports = async function (req , res, filepath) {
   try {
@@ -13,8 +20,14 @@ module.exports = async function (req , res, filepath) {
     }else if(stats.isDirectory()){
       const files = await readdir(filepath);
       res.stausCode = 200;
-      res.setHeader('Content-Type','text/plain');
-      res.end(files.join(','));
+      res.setHeader('Content-Type','text/html');
+      const dir = path.relative(config.root,filepath);
+      const data = {
+        title:path.basename(filepath),
+        dir: dir ? `/${dir}` : '' ,
+        files
+      };
+      res.end(template(data));
     }
 } catch (ex) {
       console.error(ex);
